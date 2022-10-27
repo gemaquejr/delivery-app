5a870dab7c86246c4efcb5af88d0657c367db8a5
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-27 16:36:00 -0300

    :bangbang:FEAT: Criadas as models
    	new file:   back-end/src/database/models/products.js
    	new file:   back-end/src/database/models/sales.js
    	new file:   back-end/src/database/models/salesProducts.js
    	new file:   back-end/src/database/models/users.js

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,25 @@
+const ProductsModel = (sequelize, DataTypes) => {
+  const Products = sequelize.define('products', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL(4, 2),
+    urlImage: DataTypes.STRING,
+  }, {
+    tableName: 'products',
+    timestamps: false,
+  });
+
+  Products.associate = (models) => {
+    Products.hasMany(models.SalesProducts,
+      { foreignKey: 'id', as: 'salesProducts' });
+  }
+  
+  return Products;
+};
+
+module.exports = ProductsModel;
---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,37 @@
+const SalesModel = (sequelize, DataTypes) => {
+  const Sales = sequelize.define('sales', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    sellerId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    totalPrice: DataTypes.DECIMAL(9, 2),
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATE,
+    status: DataTypes.STRING,
+  }, {
+    tableName: 'sales',
+    timestamps: false,
+    underscored: true,
+  });
+
+  Sales.associate = (models) => {
+    Sales.belongsTo(models.Users,
+      { foreignKey: 'userId', as: 'users' },
+      { foreignKey: 'sellerId', as: 'sellers' });
+  }
+  
+  return Sales;
+};
+
+module.exports = SalesModel;

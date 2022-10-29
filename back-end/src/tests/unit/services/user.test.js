70658e3ff9a61fac0e9e574205ba9ed101a81f9f
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-10-29 12:29:43 -0300

    feat: adiciona os testes da camada service de login

--- back-end/src/tests/unit/services/user.test.js
+++ back-end/src/tests/unit/services/user.test.js
@@ -1,49 +1,69 @@
 const { expect } = require('chai');
-const md5 = require('md5');
 const { describe, it } = require('mocha');
 const sinon = require('sinon');
 
-const userModel = require('../../../database/models/users');
-const userService = require('../../../database/services/users');
-const { userLoginMock, userMock } = require('../../mocks/userMock');
+const { Users }  = require('../../../database/models');
+const loginService = require('../../../api/services/loginService');
+const { userLoginMock, userMock, unregisteredUserMock, userLoginBadPassMock } = require('../../mocks/userMock');
 
-describe('users model', () => {
-  describe('logging in', () => {
-    describe('when it sucessfully', () => {
+describe('login Service', () => {
+  describe('quando faz login', () => {
+    describe('e tem sucesso', () => {
       before(() => {
-        sinon.stub(userModel, 'findOne').resolves(userMock);
+        sinon.stub(Users, 'findOne').resolves(userMock);
       });
       after(() => {
         sinon.restore();
       });
 
-      it('returns an object', async () => {
-        const user = await userService.Login(userLoginMock);
+      it('retorna um objeto', async () => {
+        const user = await loginService.login(userLoginMock);
+        
         expect(user).to.be.an('object');
       });
 
-      it('returns an object', async () => {
-        const user = await userService.userLogin(userLoginMock);
+      it('retorna um objeto com as chaves "status" e "json"', async () => {
+        const user = await loginService.login(userLoginMock);
+
         expect(user).to.have.all.keys('status', 'json');
       });
 
-      it('returns an json object with token', async () => {
-        const user = await userService.userLogin(userLoginMock);
+      it('retorna um objeto json com token e status 200', async () => {
+        const user = await loginService.login(userLoginMock);
+
         expect(user.json).to.have.property('token');
+        expect(user.status).to.be.eq(200);
       });
     });
 
-    describe('when it failed', () => {
-      it('returns an Error with status 400 and message "algum campo esta vazio"', async () => {
+    describe('quando falha', () => {
+
+      it('retorna um objeto com as chaves "status" e "json" contendo a chave "message"', async () => {
+        const user = await loginService.login({ password: 'any-pass' });
         
+        expect(user).to.be.an('object');
+        expect(user).to.be.have.all.keys('status', 'json');
+      })
+    
+      it('retorna um Erro com status 400 e mensagem "Algum campo está vazio"', async () => {
+        const user = await loginService.login({ password: 'any-pass' });
+
+        expect(user.status).to.be.eq(400);
+        expect(user.json.message).to.be.eq('Algum campo está vazio');
       });
 
-      it('returns an Error with status 404 and message "usuário não encontrado"', async () => {
-       
+      it('retorna um Erro com status 404 e mensagem "Usuário não encontrado"', async () => {
+        const user = await loginService.login(unregisteredUserMock);
+        
+        expect(user.status).to.be.eq(404);
+        expect(user.json.message).to.be.eq('Usuário não encontrado');
       });
 
-      it('returns an Error with status 401 and message "senha invalida"', async () => {
+      it('retorna um Erro com status 401 e mensagem "Senha inválida"', async () => {
+        const user = await loginService.login(userLoginBadPassMock);
         
+        expect(user.status).to.be.eq(401);
+        expect(user.json.message).to.be.eq('Senha inválida');
       });
     })
    })

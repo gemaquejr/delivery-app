e061a694019f70d1de46e0f61e0dd574ba7a1b5f
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-10-29 14:10:41 -0300

    fix: renomeando user.test para loginService.test

--- back-end/src/tests/unit/services/loginService.test.js
+++ back-end/src/tests/unit/services/loginService.test.js
@@ -17,19 +17,19 @@ describe('login Service', () => {
       });
 
       it('retorna um objeto', async () => {
-        const user = await loginService.login(userLoginMock);
+        const user = await loginService(userLoginMock.email, userLoginMock.password);
         
         expect(user).to.be.an('object');
       });
 
       it('retorna um objeto com as chaves "status" e "json"', async () => {
-        const user = await loginService.login(userLoginMock);
+        const user = await loginService(userLoginMock.email, userLoginMock.password);
 
         expect(user).to.have.all.keys('status', 'json');
       });
 
       it('retorna um objeto json com token e status 200', async () => {
-        const user = await loginService.login(userLoginMock);
+        const user = await loginService(userLoginMock.email, userLoginMock.password);
 
         expect(user.json).to.have.property('token');
         expect(user.status).to.be.eq(200);
@@ -39,28 +39,28 @@ describe('login Service', () => {
     describe('quando falha', () => {
 
       it('retorna um objeto com as chaves "status" e "json" contendo a chave "message"', async () => {
-        const user = await loginService.login({ password: 'any-pass' });
+        const user = await loginService('', 'any-pass');
         
         expect(user).to.be.an('object');
         expect(user).to.be.have.all.keys('status', 'json');
       })
     
       it('retorna um Erro com status 400 e mensagem "Algum campo está vazio"', async () => {
-        const user = await loginService.login({ password: 'any-pass' });
+        const user = await loginService('', 'any-pass');
 
         expect(user.status).to.be.eq(400);
         expect(user.json.message).to.be.eq('Algum campo está vazio');
       });
 
       it('retorna um Erro com status 404 e mensagem "Usuário não encontrado"', async () => {
-        const user = await loginService.login(unregisteredUserMock);
+        const user = await loginService(unregisteredUserMock.email, unregisteredUserMock.password);
         
         expect(user.status).to.be.eq(404);
         expect(user.json.message).to.be.eq('Usuário não encontrado');
       });
 
       it('retorna um Erro com status 401 e mensagem "Senha inválida"', async () => {
-        const user = await loginService.login(userLoginBadPassMock);
+        const user = await loginService(userLoginBadPassMock.email, userLoginBadPassMock.password);
         
         expect(user.status).to.be.eq(401);
         expect(user.json.message).to.be.eq('Senha inválida');

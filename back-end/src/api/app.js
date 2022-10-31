c6e5742c74c231b27e3a6523a07ef1033705720b
Aline Soliman <alineapreis@hotmail.com>
2022-10-31 17:32:11 -0300

    fix: fix lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
 const loginRoutes = require('./routes/login.routes');
-const productsRoutes = require('./routes/products.routes')
+const productsRoutes = require('./routes/products.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(loginRoutes);
 app.use(productsRoutes);

b20e7cf2703755a7496b69e00e8125225d2cee56
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-10-31 10:03:58 -0300

    fix: fix provider from react-redux, now the Provider wrapper the app correctly

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes as Switch, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
@@ -14,21 +16,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/login" element={ <Login /> } />
-        <Route
-          path="/"
-          element={ <Navigate to="/login" replace /> }
-        />
-        <Route path="/register" element={ <Register /> } />
-        <Route path="/customer/products" element={ <Products /> } />
-        <Route path="/customer/checkout" element={ <Checkout /> } />
-        <Route path="/customer/orders" element={ <CustomerOrders /> } />
-        <Route path="/customer/orders/:id" element={ <CustomerOrdersDetails /> } />
-        <Route path="/seller/orders" element={ <SellerOrders /> } />
-        <Route path="/seller/orders/:id" element={ <SellerOrdersDetails /> } />
-        <Route path="/admin/manage" element={ <Manage /> } />
-      </Switch>
+      <Provider store={ store }>
+        <Switch>
+          <Route path="/login" element={ <Login /> } />
+          <Route
+            path="/"
+            element={ <Navigate to="/login" replace /> }
+          />
+          <Route path="/register" element={ <Register /> } />
+          <Route path="/customer/products" element={ <Products /> } />
+          <Route path="/customer/checkout" element={ <Checkout /> } />
+          <Route path="/customer/orders" element={ <CustomerOrders /> } />
+          <Route path="/customer/orders/:id" element={ <CustomerOrdersDetails /> } />
+          <Route path="/seller/orders" element={ <SellerOrders /> } />
+          <Route path="/seller/orders/:id" element={ <SellerOrdersDetails /> } />
+          <Route path="/admin/manage" element={ <Manage /> } />
+        </Switch>
+      </Provider>
     </div>
   );
 }

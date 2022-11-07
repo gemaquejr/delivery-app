818acb7e60974954302411d48f57afe365d4365d
Wagner B <wagner_mbarbosa@hotmail.com>
2022-11-07 15:52:32 -0300

    :bangbang:FEAT: Cria o endpoint GET "/sales" para pegar todas as vendas cadastradas

--- back-end/src/api/routes/sales.routes.js
+++ back-end/src/api/routes/sales.routes.js
@@ -6,6 +6,7 @@ const { Router } = express;
 const salesRoutes = Router();
 
 salesRoutes.post('/sales', salesController.newSale);
+salesRoutes.get('/sales/', salesController.findAllSales);
 salesRoutes.get('/sales/:id', salesController.findSale);
 
 module.exports = salesRoutes;

43f154be778af5c0960e40122a534db4c6688261
Wagner B <wagner_mbarbosa@hotmail.com>
2022-11-08 18:18:20 -0300

    :bangbang:FEAT: Implementa a pagina de Gerenciamento de administrador e seus data-testid

--- front-end/src/pages/Manage.jsx
+++ front-end/src/pages/Manage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from '../components/Header';
+import NewUserForm from '../components/NewUserForm';
 
 function Manage() {
   return (
     <div>
+      <Header />
       <h2>Página de admin manage - requisito bônus- teste</h2>
+      <p data-testid="admin_manage__element-invalid-register" />
+      <div>
+        Cadastrar novo usuário
+        <NewUserForm />
+      </div>
+      <div>
+        Lista de usuários
+      </div>
     </div>
   );
 }

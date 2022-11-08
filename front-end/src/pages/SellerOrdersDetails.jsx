d8fd32e0677c6ae6cb3556011dbf4590dfc3617a
Wagner B <wagner_mbarbosa@hotmail.com>
2022-11-08 17:31:49 -0300

    :bangbang:FEAT: Implementa os componentes e seus data-testid

--- front-end/src/pages/SellerOrdersDetails.jsx
+++ front-end/src/pages/SellerOrdersDetails.jsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import TableProducts from '../components/TableProducts';
+import { getOrderDetails, getUserById } from '../services/apiHelper';
+import { orderDetails } from '../redux/reducers/productSlice';
+import OrderDetailsHeader from '../components/OrderDetailsHeader';
 import Header from '../components/Header';
 
 function SellerOrdersDetails() {
   const { id } = useParams();
+  const [orderInfo, setOrderInfo] = useState({ seller: { name: '' } });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function order() {
+      const response = await getOrderDetails(id);
+
+      const { products, sellerId } = response;
+
+      const seller = await getUserById(sellerId);
+      response.seller = seller;
+
+      const magicNumber = 10;
+      const date = response.saleDate.substring(0, magicNumber).split('-');
+      const formattedDate = `${date[2]}/${date[1]}/${date[0]}`;
+      response.saleDate = formattedDate;
+
+      setOrderInfo(response);
+
+      const productsList = [];
+      products.forEach((product) => {
+        const pd = {
+          idP: product.id,
+          nameP: product.name,
+          quantidadeP: product.SalesProducts.quantity,
+          priceP: product.price,
+        };
+        productsList.push(pd);
+      });
+      dispatch(orderDetails(productsList));
+    }
+
+    order();
+  }, [dispatch, id]);
 
   return (
     <div>
       <Header />
       <h2>
-        Página de Pedidos
-        { id }
-        - Teste
+        Detalhe do Pedido
       </h2>
+      <OrderDetailsHeader
+        page="seller_order_details"
+        orderId={ orderInfo.id }
+        sellerName={ orderInfo.seller.name }
+        saleDate={ orderInfo.saleDate }
+        status={ orderInfo.status }
+      />
+      <TableProducts page="seller_order_details" />
     </div>
   );
 }
